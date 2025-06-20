9dcbcad1932e842578841f8cc14a74c2afefcc43
sravsguda123 <136485415+sravsguda123@users.noreply.github.com>
2025-06-20 15:05:55 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Virutal-classroom---A1/'
   plugins: [react() , tailwindcss()],
 })
